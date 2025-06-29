45044113e5258f47311928b50239fc891402990b
Masterpuff77 <55856269+Masterpuff77@users.noreply.github.com>
2025-06-29 20:17:42 +0300

    Fix React DOM import error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react-dom': 'react-dom/client'
+    }
+  },
   optimizeDeps: {
     include: ['react-dom/client'],
     exclude: ['lucide-react', '@daily-co/daily-react'],
